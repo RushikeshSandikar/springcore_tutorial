9991e61707ddc4df9482a8cfbcc158b8018830e7
Rushikesh Sandikar <rushi46sandikar@gmail.com>
2023-12-18 18:15:03 +0530

    springcore file

---
+++ src/main/java/com/springcore/spel/Test.java
@@ -0,0 +1,21 @@
+package com.springcore.spel;
+
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.expression.Expression;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext con= new ClassPathXmlApplicationContext("com/springcore/spel/config.xml");
+		Demo d1= con.getBean("demo",Demo.class);
+		System.out.println(d1);
+		
+		
+
+	}
+
+}
